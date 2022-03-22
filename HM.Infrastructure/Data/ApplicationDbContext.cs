a14810ddba0d8b63ec0fd79cb68342838ca2d220
Ivanov22mitko <mita6ki.com@gmail.com>
2022-03-22 23:52:36 +0200

    Data folder moved to Infrastructure.

--- HM.Infrastructure/Data/ApplicationDbContext.cs
+++ HM.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Hospital_management.Data
+namespace HM.Infrastructure
 {
     public class ApplicationDbContext : IdentityDbContext
     {
