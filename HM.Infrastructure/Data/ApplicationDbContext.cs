1f07f032bf8c0ef2b0bebe33099d7b7ff010a248
Ivanov22mitko <mita6ki.com@gmail.com>
2022-04-01 17:06:55 +0300

    IdentityUser extended.

--- HM.Infrastructure/Data/ApplicationDbContext.cs
+++ HM.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HM.Infrastructure.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HM.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
